e8aad3f1b79ba1ae858a87d6db78eb670787c7fe
Yuri-Diego <contatoprofissional.ya@gmail.com>
2025-01-23 21:45:27 -0300

    Homolog Ok

--- src/main/java/com/springtraining/dslist/projections/GameMinProjection.java
+++ src/main/java/com/springtraining/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.springtraining.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
